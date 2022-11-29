cd96a0cecef65cc53e406e67f9b976f55f589974
Alexander Mysak <reactionnnnn@gmail.com>
2022-11-29 21:51:08 +0200

    Theme, gitignore

--- mackup/.config/nvim/lua/razen/core/colorscheme.lua
+++ mackup/.config/nvim/lua/razen/core/colorscheme.lua
@@ -1,7 +1,24 @@
--- set colorscheme to poimandres with protected call
--- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfox")
-if not status then
-	print("Colorscheme not found!") -- print error if colorscheme not installed
-	return
-end
+require("nightfox").setup({
+	options = {
+		compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+		compile_file_suffix = "_compiled", -- Compiled file suffix
+		transparent = true, -- Disable setting background
+		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+		dim_inactive = false, -- Non focused panes set to alternative background
+		module_default = true, -- Default enable value for modules
+		styles = { -- Style to be applied to different syntax groups
+			comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+			conditionals = "NONE",
+			constants = "NONE",
+			functions = "NONE",
+			keywords = "NONE",
+			numbers = "NONE",
+			operators = "NONE",
+			strings = "NONE",
+			types = "NONE",
+			variables = "NONE",
+		},
+	},
+})
+
+vim.cmd("colorscheme nightfox")
