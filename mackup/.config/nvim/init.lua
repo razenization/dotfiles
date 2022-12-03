821a42e8fc9c6056895af95ed7f9747c247c015f
Alexander Mysak <reactionnnnn@gmail.com>
2022-12-03 05:02:04 +0200

    adding astro vim to try out

--- mackup/.config/nvim/init.lua
+++ mackup/.config/nvim/init.lua
@@ -1,16 +1,21 @@
-require("razen.plugins-setup")
-require("razen.core.options")
-require("razen.core.keymaps")
-require("razen.core.colorscheme")
-require("razen.plugins.comment")
-require("razen.plugins.nvim-tree")
-require("razen.plugins.lualine")
-require("razen.plugins.telescope")
-require("razen.plugins.nvim-cmp")
-require("razen.plugins.lsp.mason")
-require("razen.plugins.lsp.lspsaga")
-require("razen.plugins.lsp.lspconfig")
-require("razen.plugins.lsp.null-ls")
-require("razen.plugins.autopairs")
-require("razen.plugins.treesitter")
-require("razen.plugins.gitsigns")
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then impatient.enable_profile() end
+
+for _, source in ipairs {
+  "core.utils",
+  "core.options",
+  "core.bootstrap",
+  "core.diagnostics",
+  "core.autocmds",
+  "core.mappings",
+  "configs.which-key-register",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
+
+if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
+  vim.schedule(function() astronvim.notify("Unsupported Neovim Version! Please check the requirements", "error") end)
+end
